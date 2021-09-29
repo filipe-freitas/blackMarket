b7bf505404be5e6bdb7316d6456aaa3d23df8a04
Filipe Freitas <filipe.freitas@live.com>
2021-09-29 15:22:14 -0300

    feat: create a service to creating an order

---
+++ src/services/CreateOrderService.ts
@@ -0,0 +1,29 @@
+import validate from "../ValidateDocumentNumber";
+
+interface CreateOrderDTO {
+  userDocument: string;
+  products: {
+    productId: string;
+    productQty: number;
+    productPrice: number;
+  }[];
+  discountValue: number;
+}
+
+interface OrderDTO {
+  totalPrice: number;
+}
+
+class CreateOrderService {
+  public async execute({userDocument, products, discountValue}: CreateOrderDTO): Promise<OrderDTO> { 
+    if (!validate(userDocument)) throw new Error("Número de documento do usuário inválido");
+
+    let valorTotal = products.reduce((acc, preco) => {
+      return (preco.productPrice * preco.productQty) + acc
+    }, 0);
+
+    return {totalPrice: valorTotal - discountValue};
+  }
+}
+
+export default CreateOrderService;
