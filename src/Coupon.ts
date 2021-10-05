daf6e67a8874f8f3c28f34fee096a626428a1c3b
Filipe Freitas <filipe.freitas@live.com>
2021-10-05 19:05:14 -0300

    feat: add a method to verify if a coupon is valid

--- src/Coupon.ts
+++ src/Coupon.ts
@@ -2,8 +2,14 @@ export default class Coupon {
   description: string;
   percentage: number;
 
-  constructor(description: string, percentage: number){
+  constructor(description: string, percentage: number) {
+    if(!this.isValid(description, percentage)) throw new Error('Invalid coupon');
     this.description = description;
     this.percentage = percentage;
+  }
+
+  isValid(description: string, percentage:number): boolean {
+    if (description === 'VALE10') return false;
+    return true;
   }
 }
