dd1a53a905438289af92e2937661353406d5782b
Filipe Freitas <filipe.freitas@live.com>
2021-10-13 20:28:12 -0300

    feat: return more data from the item
    Add the possibility to get the volume and density from an item.

--- src/Item.ts
+++ src/Item.ts
@@ -1,11 +1,19 @@
 export default class Item {
-  id: string;
-  description: string;
-  price: number;
 
-  constructor(id: string, description: string, price: number) {
-    this.id = id;
-    this.description = description;
-    this.price = price;
+  constructor(
+    readonly id: string,
+    readonly description: string,
+    readonly price: number,
+    readonly weight: number = 0,
+    readonly height: number = 0,
+    readonly width: number = 0,
+    readonly depth: number = 0) {}
+
+  getVolume() {
+    return this.height/100 * this.width/100 * this.depth/100;
+  }
+
+  getDensity() {
+    return parseFloat((this.weight/this.getVolume()).toPrecision(5));
   }
 }
